214ab77b85d39a7f1cec323f4f3696d3b64a50fe
jbandera <julia.bandera@student.kuleuven.be>
2023-10-26 13:56:44 +0200

    structure of the functions for selecting the cavity closes to the ligand

--- main.py
+++ main.py
@@ -15,5 +15,29 @@ def load_mol_file(filename):
     return df
 
 
+def center_of_gravity(filename):
+    """
+
+    :param filename: file name of the structure (mol2 format)
+    :return: center of gravity for the given structure
+    """
+    # Load the mol file (with the load_mol_file function)
+    atoms = load_mol_file(filename)
+    # Calculate the center of gravity of the structure
+    pass
+
+
+def select_cavity():
+    """
+    Investigates all cavities found with Volsite (with ligand restriction) and selects the one closest
+    to the center of gravity of the ligand.
+
+    :return: df of the cavity closest to the ligand
+    """
+    # compare the distance from the cavities to the ligand and return the closest cavity
+    pass
+
+
 if __name__ == '__main__':
-    cavity = load_mol_file("volsite/CAVITY_N1_ALL.mol2")
+    cavity = load_mol_file("1a28/volsite/CAVITY_N1_ALL.mol2")
+    print(cavity)
