8cc8c692936e77a776067612db4ab9e5a3d99506
jbandera <julia.bandera@student.kuleuven.be>
2023-10-31 14:10:51 +0100

    retrieval of volsite descriptors

--- main.py
+++ main.py
@@ -38,6 +38,27 @@ def select_cavity():
     pass
 
 
+def get_volsite_descriptors(filename, cavity_num):
+    """
+    Loads the descriptors for a given cavity returned by Volsite into a pandas dataframe.
+
+    :param filename: file name with volsite descriptors (.txt)
+    :param cavity_num: number of the selected cavity (the one closer to the ligand)
+    :return: dataframe with volsite descriptors for a given cavity
+    """
+    # add a failsafe for the incorrect cavity_num later!!!
+    points = ['CZ', 'CA', 'O', 'OD1', 'OG', 'N', 'NZ', 'DU']
+    column_names = ['volume'] + points
+    for point in points:
+        column_names += [f'{point}_below_40', f'{point}_between_40_50', f'{point}_between_50_60', f'{point}_between_60_70',
+                         f'{point}_between_70_80', f'{point}_between_80_90', f'{point}_between_90_100',
+                         f'{point}_between_100_110', f'{point}_between_110_120', f'{point}_120']
+    column_names += ['name']
+    df = pd.read_csv(filename, sep=" ", index_col=False, header=None, names=column_names)
+    return df.loc[cavity_num-1, df.columns != 'name']
+
+
 if __name__ == '__main__':
-    cavity = load_mol_file("1a28/volsite/CAVITY_N1_ALL.mol2")
-    print(cavity)
+    cavity = load_mol_file("../1a28/volsite/CAVITY_N1_ALL.mol2")
+    volsite_descriptors = get_volsite_descriptors("../1a28/volsite/1a28_prot_no_waters_descriptor.txt", 1)
+    print(volsite_descriptors)
