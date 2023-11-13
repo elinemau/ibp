28e65a838259f7f547cd553bd39029cb33f4426f
jbandera <julia.bandera@student.kuleuven.be>
2023-11-13 14:39:14 +0100

    maximum distance between two cavity points (pharmacophore & cavity files)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from itertools import combinations_with_replacement
 
 
 def load_mol_file(filename):
@@ -9,7 +11,11 @@ def load_mol_file(filename):
             line = file.readline()
         line = file.readline()
         while not line.startswith("@<TRIPOS>BOND"):
-            df.loc[len(df)] = line.strip().split()
+            data = line.strip().split()
+            # Convert 'x', 'y', and 'z' columns to float
+            data[2:5] = map(float, data[2:5])
+            data[-1] = map(float, data[-1])
+            df.loc[len(df)] = data
             line = file.readline()
     file.close()
     return df
@@ -58,7 +64,41 @@ def get_volsite_descriptors(filename, cavity_num):
     return df.loc[cavity_num-1, df.columns != 'name']
 
 
+def max_dist_cavity_points(cavity):
+    """
+    Calculates the maximum distance between every combination of two cavity points ('CZ', 'CA', 'O', 'OD1', 'OG', 'N',
+    'NZ', 'DU') and returns the values as a pandas dataframe.
+
+    :param cavity: pandas dataframe representing a cavity (contains information on atoms / cavity points)
+    :return: pandas dataframe with maximum distances between pairs of cavity points
+    """
+    grouped_cavity = cavity.groupby('atom_name')
+
+    point_types = ['CZ', 'CA', 'O', 'OD1', 'OG', 'N', 'NZ', 'DU']
+    point_types.sort()
+
+    max_distances = pd.DataFrame(0.0, columns=list(combinations_with_replacement(point_types, 2)), index=[0])
+
+    for (atom_type1, group1), (atom_type2, group2) in combinations_with_replacement(grouped_cavity, 2):
+        pair = (atom_type1, atom_type2)
+
+        # Extract coordinates directly from the DataFrame
+        coords1 = group1[['x', 'y', 'z']].values
+        coords2 = group2[['x', 'y', 'z']].values
+
+        # Calculate pairwise distances without using np.newaxis
+        dist_matrix = np.linalg.norm(coords1[:, None, :] - coords2, axis=-1)
+
+        max_distance = dist_matrix.max()
+
+        # Update max_distances directly
+        max_distances.at[0, pair] = max(max_distances.at[0, pair], max_distance)
+
+    return max_distances
+
+
 if __name__ == '__main__':
     cavity = load_mol_file("../1a28/volsite/CAVITY_N1_ALL.mol2")
     volsite_descriptors = get_volsite_descriptors("../1a28/volsite/1a28_prot_no_waters_descriptor.txt", 1)
-    print(volsite_descriptors)
+    # print(volsite_descriptors)
+    print(max_dist_cavity_points(cavity))
