871870ebd4476f0c2e3cbb4e3be8d678eb286342
jbandera <julia.bandera@student.kuleuven.be>
2023-11-22 18:59:45 +0100

    maximum area of triangle formed by triplets of cavity points (pharmacophore & cavity files)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from itertools import combinations_with_replacement
+from itertools import combinations, combinations_with_replacement, product
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
@@ -146,6 +146,85 @@ def max_dist_cavity_points(cavity):
         max_distances.at[0, pair] = max(max_distances.at[0, pair], max_distance)
 
     return max_distances
+
+
+def distance(point1, point2):
+    """
+    Calculates the distance between two points in 3D space.
+
+    :param point1: first point in 3D space
+    :param point2: second point in 3D space
+    :return: distance between point1 and point2
+    """
+    return math.sqrt(sum((p1 - p2)**2 for p1, p2 in zip(point1, point2)))
+
+
+def is_valid_triangle(side1, side2, side3):
+    """
+    Checks if the given sides can form a valid triangle.
+
+    :param side1: first side of the triangle
+    :param side2: second side of the triangle
+    :param side3: third side of the triangle
+    :return: true if given sides can form a valid triangle, false otherwise
+    """
+    return (side1 + side2 > side3) and (side2 + side3 > side1) and (side3 + side1 > side2)
+
+
+def calculate_triangle_area(coord1, coord2, coord3):
+    """
+    Calculates the area of a triangle using Heron's formula.
+
+    :param coord1: coordinates of the first point in 3D space
+    :param coord2: coordinates of the second point in 3D space
+    :param coord3: coordinates of the third point in 3D space
+    :return: the area of the triangle formed by the three given points or 0 if the points do not form a valid triangle
+    """
+    # Calculate all sides of the triangle
+    side1 = distance(coord1, coord2)
+    side2 = distance(coord2, coord3)
+    side3 = distance(coord3, coord1)
+
+    if is_valid_triangle(side1, side2, side3):
+        # Semiperimeter
+        s = (side1 + side2 + side3) / 2
+
+        # Heron's formula
+        area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+        return area
+    else:
+        return 0
+
+
+def max_triplet_area(cavity):
+    """
+    Calculates the maximum area of a triangle formed by every combination of three cavity points ('CZ', 'CA', 'O',
+    'OD1', 'OG', 'N', 'NZ', 'DU') and returns the values as a pandas dataframe.
+
+    :param cavity: pandas dataframe representing a cavity (contains information on atoms / cavity points)
+    :return: pandas dataframe with maximum area of a triangle formed by triplets of cavity points
+    """
+    grouped_cavity = cavity.groupby('atom_name')
+    point_types = ['CZ', 'CA', 'O', 'OD1', 'OG', 'N', 'NZ', 'DU']
+    point_types.sort()
+
+    # Note: function combinations instead of combinations_with_replacement is used to save computational time
+    max_areas = pd.DataFrame(0.0, columns=list(combinations(point_types, 3)), index=[0])
+
+    for triplet_combination in combinations(grouped_cavity, 3):
+        triplet = tuple(sorted([atom_type for atom_type, _ in triplet_combination]))
+
+        # Extract coordinates from the DataFrame
+        all_coords = [group[['x', 'y', 'z']].values for _, group in triplet_combination]
+        # Create triplets of coordinates
+        coord_triplets = product(*all_coords)
+
+        for triangle in coord_triplets:
+            # Update max_areas directly
+            max_areas.at[0, triplet] = max(max_areas.at[0, triplet], calculate_triangle_area(*triangle))
+
+    return max_areas
+
 
 def pc_retrieval(df):
     """
