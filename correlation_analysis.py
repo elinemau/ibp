1929403a7058af6d1487aba9451f36ac7d166a77
r0934354 <6667>
2023-11-28 13:28:21 +0100

    added p-value filtering to correlation

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -9,9 +9,9 @@ df = pd.read_excel("C:/Users/32496/OneDrive - KU Leuven/kuleuven(1)/IBP/datafram
 df.set_index("protein_code", inplace=True)
 correlation = df.dropna().corr()
 p_values = df.dropna().corr(method=lambda x,y: pearsonr(x,y)[1])
-correlation.to_excel("C:/Users/32496/OneDrive - KU Leuven/kuleuven(1)/IBP/correlation.xlsx", index=True)
-p_values.to_excel("C:/Users/32496/OneDrive - KU Leuven/kuleuven(1)/IBP/p_values.xlsx")
-print(p_values)
-sns.heatmap(correlation, vmin=-1, vmax=1, cmap="rocket_r")
-plt.show()
+#correlation.to_excel("C:/Users/32496/OneDrive - KU Leuven/kuleuven(1)/IBP/correlation.xlsx", index=True)
+significant_correlations = correlation[p_values <0.05]
+print(significant_correlations)
+#sns.heatmap(correlation, vmin=-1, vmax=1, cmap="rocket_r")
+#plt.show()
 
